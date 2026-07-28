9b063ea2b8f2917fbb8e364c9831cf5743a83b90
Yevhen <e_danilchenko@outlook.com>
2026-07-28 16:48:24 +0300

    #1 add register new user API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'regex:/^[A-Za-z0-9]+$/', 'unique:users,name'],
+            'password' => ['required', 'string', 'min:4'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'password' => $request->password,
+        ]);
+
+        $token = $user->createToken('chat-token')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user,
+        ], 201);
+    }
+
     public function login(Request $request)
     {
         $request->validate([
